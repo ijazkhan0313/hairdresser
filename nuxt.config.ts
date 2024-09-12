81d393b1cae98c907d36c8d6148d2cc7ea1ad69b
ijazkhan0313 <96014060+ijazkhan0313@users.noreply.github.com>
2024-09-12 17:48:23 +0500

    asdf

--- nuxt.config.ts
+++
@@ -1,7 +0,0 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
-export default defineNuxtConfig({
-  modules: ["@pinia/nuxt", "usebootstrap", '@pinia/nuxt', '@pinia-plugin-persistedstate/nuxt'],
-  // css: [
-  //   'bootstrap/dist/css/bootstrap.min.css'
-  // ]
-})
